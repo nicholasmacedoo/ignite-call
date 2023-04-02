f44f6d19b7a52e3901faef586d9cfb8041645f54
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2023-04-02 12:10:42 -0300

    :sparkles: create page scheduler for appointment

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,32 @@
+import { Calendar } from '../../../../../components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+  const isDateSelected = false
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de abril</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,16 @@
-export function getWeekDays() {
+interface GetWeekDaysProps {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysProps = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from({ length: 7 })
     .map((_, day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekday) =>
-      weekday.substring(0, 1).toUpperCase().concat(weekday.substring(1)),
-    )
+    .map((weekday) => {
+      if (short) {
+        return weekday.substring(0, 3).toUpperCase()
+      }
+      return weekday.substring(0, 1).toUpperCase().concat(weekday.substring(1))
+    })
 }
